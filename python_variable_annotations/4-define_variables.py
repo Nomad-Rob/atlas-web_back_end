5015cf07dc71454287fa0234bd285bddb262ee27
Nomad-Rob <rfarley89@gmail.com>
2024-01-02 16:08:18 -0600

    task 4 complete

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""Define and annotate the following variables with the specified values:"""
+
+
+a = 1 # int
+pi = 3.14 # float
+i_understand_annotations = True # bool
+school = "Holberton" # str
