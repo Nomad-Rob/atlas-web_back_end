d544db417c9afa6ccd7da216d9e9c2dde0162520
Nomad-Rob <rfarley89@gmail.com>
2024-01-02 16:09:59 -0600

    adding spaces for pycodestyle inline comments

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -3,7 +3,7 @@
 """Define and annotate the following variables with the specified values:"""
 
 
-a = 1 # int
-pi = 3.14 # float
-i_understand_annotations = True # bool
-school = "Holberton" # str
+a = 1  # int
+pi = 3.14  # float
+i_understand_annotations = True  # bool
+school = "Holberton"  # str
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3
+
+"""Write a type-annotated function sum_list which takes a list input_list"""
