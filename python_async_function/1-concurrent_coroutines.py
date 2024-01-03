32387d4bedc6d5d1076fcc2cc498daae7310c1e4
Nomad-Rob <rfarley89@gmail.com>
2024-01-03 12:38:08 -0600

    fix task 1 and task 2 complete

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -18,6 +18,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return list of all delays in ascending order"""
-    delays = [await wait_random(max_delay) for i in range(n)]
-    delays.sort()
-    return delays
+    delays = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+"""From the previous file, import wait_random into 2-measure_runtime.py.
+Create a measure_time function with integers n and max_delay as arguments
+that measures the total execution time for wait_random(n, max_delay), and
+returns total_time / n. Your function should return a float."""
+
+
+import asyncio
+import time
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Return average time"""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    return (end - start) / n
