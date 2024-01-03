341fbc8f382c93d882e21954ae5c0f3180941d23
Nomad-Rob <rfarley89@gmail.com>
2024-01-03 14:47:03 -0600

    adding gitignore. and task 1 complete

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+"""Import async_generator from the previous task and
+then write a coroutine called async_comprehension that takes
+no arguments."""
+
+
+import asyncio
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """Collect 10 random numbers using an async comprehensing over async_generator"""
+    return [i async for i in async_generator()]
