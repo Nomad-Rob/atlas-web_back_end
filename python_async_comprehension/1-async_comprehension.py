52674bdb32044806b9d8812b219ac317a5c5f84d
Nomad-Rob <rfarley89@gmail.com>
2024-01-03 14:55:34 -0600

    fixing task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,11 +5,11 @@ then write a coroutine called async_comprehension that takes
 no arguments."""
 
 
-import asyncio
 from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    """Collect 10 random numbers using an async comprehensing over async_generator"""
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehensing over
+    async_generator"""
     return [i async for i in async_generator()]
