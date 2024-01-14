68de83acad737703f163e4b48136f1a3fc4103ba
Nomad-Rob <rfarley89@gmail.com>
2024-01-14 10:28:15 -0600

    First attempt with task 1. Added the class redactingFormatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
-"""Write a functioncalled filter_datum that returns thelog message obfuscated:
-Arguments:
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character is separating all
-    fields in the log line (message)
-The function should use a regex to replace occurrences of certain field values.
-filter_datum should be less than 5 lines long and use re.sub to perform the
-substitution with a single regex.
-"""
+"""Update the class to accept a list of strings fields
+constructor argument. Implement the format method to 
+filter values in incoming log records using filter_datum.
+Values for fields in fields should be filtered
+Do NOT extrapolate FORMAT manually. The format method
+should be less than 5 lines"""
 
 from typing import List
+import logging
 import re
 
 
@@ -23,3 +19,25 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=.*?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = record.getMessage()
+        redacted_message = filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR)
+        record.msg = redacted_message
+        return super().format(record)
