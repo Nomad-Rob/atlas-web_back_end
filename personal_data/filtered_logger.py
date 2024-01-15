08f156ff48da8679ba0d59b8f51b220c8913f15f
Nomad-Rob <rfarley89@gmail.com>
2024-01-15 10:47:49 -0600

    task 2 complete, addede get_logger and PII Fields

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,6 +11,8 @@ from typing import List
 import logging
 import re
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -43,3 +45,14 @@ class RedactingFormatter(logging.Formatter):
             self.SEPARATOR)
         record.msg = redacted_message
         return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
