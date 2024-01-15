de032e3c116cbf5f348a9a620df5ddbc81594c21
Nomad-Rob <rfarley89@gmail.com>
2024-01-15 13:07:56 -0600

    setup for task 3. need to run pwd

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,8 +10,10 @@ should be less than 5 lines"""
 from typing import List
 import logging
 import re
+import os
+import mysql.connector
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -24,19 +26,19 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class that inherits from logging.Formatter """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ constructor method for RedactingFormatter class"""
+        """ Constructor """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filter values in incoming log records using filter_datum"""
+        """ Filter values in incoming log records """
         original_message = record.getMessage()
         redacted_message = filter_datum(
             self.fields,
@@ -53,6 +55,18 @@ def get_logger() -> logging.Logger:
     logger.setLevel(logging.INFO)
     logger.propagate = False
     handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connects to the MySQL database and returns a connection object."""
+
+    return mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
