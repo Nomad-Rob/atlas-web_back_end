ac5d15480879acb639b749b0082ec66cb02959c9
Nomad-Rob <rfarley89@gmail.com>
2024-01-22 22:51:19 +0000

    easy now, task 4 complete. updated auth.py by adding session cookie method. also pycodefix on session auth.py

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -31,3 +31,9 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Session cookie"""
+        if request is None:
+            return None
+        return request.cookies.get('session_id')
