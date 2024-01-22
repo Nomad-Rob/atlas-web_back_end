b6d9629de7d70b58d349739d4f0e5fe52ee7471f
Nomad-Rob <rfarley89@gmail.com>
2024-01-22 11:35:29 -0600

    Not sure whats causing the 500 error

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -54,6 +56,7 @@ def before_request_func() -> str:
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
+    
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
