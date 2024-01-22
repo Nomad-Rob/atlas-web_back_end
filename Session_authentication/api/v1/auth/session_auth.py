220f0a7d5a7c249b1516c6e38d79ce9f5a9b039e
Nomad-Rob <rfarley89@gmail.com>
2024-01-22 22:36:18 +0000

    task 1 adding sessionAuth for creating new auth mechanism

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Session authentication will be created here"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class. """
+    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id """
+        from uuid import uuid4
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
