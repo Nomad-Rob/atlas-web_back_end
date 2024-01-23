cddcce15ac0074fc059b9b7190788e25801d3ed5
Nomad-Rob <rfarley89@gmail.com>
2024-01-23 19:46:48 +0000

    made login a string :(

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -9,31 +9,26 @@ import os
 
 
 @app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """login handles all routes for the Session authentication"""
+def login() -> str:
+    """ POST /auth_session/login
+    Return:
+      - User object JSON represented
+    """
     email = request.form.get('email')
-    password = request.form.get('password')
-
     if not email:
         return jsonify({"error": "email missing"}), 400
-
+    password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
-
     try:
         user = User.search({'email': email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    for u in user:
-        if not u.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-
+    if not user or not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user[0].id)
+    cookie_name = os.getenv('SESSION_NAME')
     response = make_response(user[0].to_json())
-    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+    response.set_cookie(cookie_name, session_id)
     return response
