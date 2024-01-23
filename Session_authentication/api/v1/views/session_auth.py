ac0943edc8fa489b2b1af48cba41820ea14cb8d3
Nomad-Rob <rfarley89@gmail.com>
2024-01-23 19:40:36 +0000

    pycode fixes

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -5,32 +5,35 @@
 from api.v1.auth.auth import Auth
 from models.user import User
 from flask import request, jsonify, abort, make_response
+import os
+
 
 @app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-
 def login():
-    """ POST /api/v1/auth_session/login
-    Return:
-      - User object JSON represented
-    """
+    """login handles all routes for the Session authentication"""
     email = request.form.get('email')
+    password = request.form.get('password')
+
     if not email:
         return jsonify({"error": "email missing"}), 400
-    password = request.form.get('password')
+
     if not password:
         return jsonify({"error": "password missing"}), 400
+
     try:
         user = User.search({'email': email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
+
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
+
     for u in user:
         if not u.is_valid_password(password):
             return jsonify({"error": "wrong password"}), 401
-        from api.v1.app import auth
-        session_id = auth.create_session(u.id)
-        response = make_response(u.to_json())
-        response.set_cookie(os.getenv('SESSION_NAME'), session_id)
-        return response
-  
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    response = make_response(user[0].to_json())
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+    return response
