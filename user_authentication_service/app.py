27c91f035f94b90e5204f4019c0a217d932a2054
Nomad-Rob <rfarley89@gmail.com>
2024-01-24 13:12:12 -0600

    setup for task 7 adding an endpoint for users

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -16,3 +16,17 @@ def hello_world() -> str:
       - the status of the API
     """
     return jsonify({"message": "Bienvenue"})
+  
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    Return:
+      - the status of the API
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
