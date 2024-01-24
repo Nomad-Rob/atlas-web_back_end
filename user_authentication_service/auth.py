ce62eabcc657e950e81c1ac4592da47e31bc775b
Nomad-Rob <rfarley89@gmail.com>
2024-01-24 11:51:48 -0600

    task 5 complete. implemented Auth class and added methods register user and hass password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,15 +2,36 @@
 
 """Auth class to interact with the authentication database."""
 
+import bcrypt
 from db import DB
-from user import User
-from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
-from uuid import uuid4
-from typing import Union
 
 
-def _hash_password(password: str) -> str:
-    """Returns a salted hash of the input password."""
-    import bcrypt
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+def _hash_password(password: str) -> bytes:
+    '''Returns a hashed version of passed in pwd'''
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'),
+                                    bcrypt.gensalt())
+    return hashed_password
+
+
+class Auth():
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str,
+                      password: str) -> User:
+        '''Returns user object after creating
+        user, if they already exits then raises
+        a ValueError'''
+        try:
+            already_exist = self._db.find_user_by(email=email)
+            if already_exist:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
