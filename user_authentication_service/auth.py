1f2e1aa27025ea2e831e228d3ecad8988384a1ef
Nomad-Rob <rfarley89@gmail.com>
2024-01-24 14:42:03 -0600

    Task 12 complete, Added method to get user from session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -62,3 +62,13 @@ class Auth():
             return user.session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        '''Returns user email'''
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return None
