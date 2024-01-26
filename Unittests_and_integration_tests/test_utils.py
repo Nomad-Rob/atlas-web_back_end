7ec4d1702df3c966fe17d10a80a5ccfb50e31649
Nomad-Rob <rfarley89@gmail.com>
2024-01-26 14:36:56 -0600

    Task 0 complete. Implamented 2 methods, test access nested map and parameterized expand in the test access netedmap class

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+""" Unittests and integration tests module """
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing nested map function """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access nested map function """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test access nested map function """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
