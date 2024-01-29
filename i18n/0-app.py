058b25925f0f8bfac10c381e472dc96851d1e2c7
Nomad-Rob <rfarley89@gmail.com>
2024-01-29 16:22:00 -0600

    Task 0 setup complete. Flask app setup and html with correct outputs

---
+++ i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Setting up basic Flask app"""
+from flask import Flask, render_template, request, g
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """GET method for index.html"""
+    return render_template('0-index.html')
+  
+
+if __name__ == "__main__":
+    app.run()
