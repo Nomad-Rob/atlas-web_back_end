7e24def2956d3b4337685f01a00903054be2ad8d
Nomad-Rob <rfarley89@gmail.com>
2024-01-29 09:45:55 -0600

    adding mock HTTP Calls and Parameterize and patch tests.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,42 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test access nested map function """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+            
+
+class TestGetJson(unittest.TestCase):
+    """ Class for testing get json function """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test get json function """
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get', return_value=mock):
+            self.assertEqual(get_json(test_url), test_payload)
+            mock.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class for testing memoize decorator """
+
+    def test_memoize(self):
+        """ Test memoize function """
+        class TestClass:
+            """ Test class """
+            def a_method(self):
+                """ Method to always return 42 """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Method to always return 42 """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            test = TestClass()
+            self.assertEqual(test.a_property, mock.return_value)
+            self.assertEqual(test.a_property, mock.return_value)
+            mock.assert_called_once()
