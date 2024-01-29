4ef26d0dbf4845765a8ce70b0e4dbcd184220794
Nomad-Rob <rfarley89@gmail.com>
2024-01-29 16:58:54 -0600

    task 1 complete with basic babel setup

---
+++ i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""Task 2"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """GET method for index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
