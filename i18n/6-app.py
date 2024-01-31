1b0b3baa20d19d642eedf315552cac0275ac8a7e
Nomad-Rob <rfarley89@gmail.com>
2024-01-31 11:08:56 -0600

    pycode style fix

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -59,7 +59,8 @@ def get_locale():
         user_locale = g.user.get('locale')
         if user_locale and user_locale in app.config['LANGUAGES']:
             return user_locale
-    header_locale = request.accept_languages.best_match(app.config['LANGUAGES'])
+    header_locale = request.accept_languages.best_match(
+      app.config['LANGUAGES'])
     if header_locale:
         return header_locale
     return app.config['BABEL_DEFAULT_LOCALE']
