8d407b538b4bb128766d3f0854a99e2a83903ef6
Nomad-Rob <rfarley89@gmail.com>
2024-02-02 15:15:53 -0600

    Changed some syntax

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,8 +7,8 @@ from typing import Union, Callable
 from functools import wraps
 
 
-def count_calls(method: Callable[..., Union[str, bytes, int, float]]) -> Callable[..., Union[str, bytes, int, float]]:
-    """Decorator function that counts how many times a function is called"""
+def count_calls(method: Callable) -> Callable:
+    """Decorator that counts how many times a function is called"""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """Wrapper function"""
@@ -20,29 +20,29 @@ def count_calls(method: Callable[..., Union[str, bytes, int, float]]) -> Callabl
 class Cache:
     """Cache class"""
     def __init__(self):
-        """Constructor method"""
+        """Constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store method that takes a data argument and returns a key"""
+        """Store data in cache"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable[..., Union[str, bytes, int, float]] = None) -> Union[str, bytes, int, float]:
-        """Get method that takes a key string and an optional callable"""
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Get data from cache"""
         data = self._redis.get(key)
         if fn:
             return fn(data)
         return data
 
     def get_str(self, data: bytes) -> str:
-        """Convert bytes to string method"""
+        """Convert bytes to str"""
         return data.decode('utf-8')
 
     def get_int(self, data: bytes) -> int:
-        """Convert bytes to int method"""
+        """Convert bytes to int"""
         return int.from_bytes(data, byteorder='big')
 
