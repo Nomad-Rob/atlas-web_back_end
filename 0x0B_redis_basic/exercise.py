743432830fde5c3219a034344ab3149de06bc926
Nomad-Rob <rfarley89@gmail.com>
2024-02-02 10:43:54 -0600

    Created the main files. Completed task 0, created a cache class to store an key. Also started redis server

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+"""Redis exercise"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self):
+        """Constructor method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store method that takes a data argument and returns a key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+  
