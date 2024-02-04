3d457d73d8e2d184672fd2c51defd4badccfc45e
Nomad-Rob <rfarley89@gmail.com>
2024-02-04 13:49:38 -0600

    changed name syntax

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 -- script should be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255)
 );
