db4bd7789beaac9c9fe3556d8e8c2746929f313d
Nomad-Rob <rfarley89@gmail.com>
2024-02-06 16:55:13 -0600

    redoing task 7 syntqax

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,13 +3,11 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10,2);
-
-    -- Calculate the average score for the given user
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-
-    -- Update the average score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END //
-
+    DECLARE average_score FLOAT;
+    -- Compute the average score
+    SELECT AVG(score) INTO average_score FROM corrections WHERE corrections.user_id = user_id;
+    -- Update the average score
+    UPDATE users SET average_score = average_score WHERE id = user_id;
+END;
+//
 DELIMITER ;
