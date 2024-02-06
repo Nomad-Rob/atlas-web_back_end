04d79d53e1bb9d22769132ace3b433d835d3e1e7
Nomad-Rob <rfarley89@gmail.com>
2024-02-06 13:27:47 -0600

    addign task 1 commplete

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- Task 1 creates a table users following these requirements:
+-- id, integer, not null, autoincrement, primary key
+-- email, string, not null, unique
+-- name, string
+-- country, enum, US, CO, TN, not null, default US
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
