06286545d4e9a7bb6b6776bb09325b244cadd89a
Nomad-Rob <rfarley89@gmail.com>
2024-02-06 16:33:36 -0600

    completed task 10 created a function that divides and returns frist number from second number

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Task 10 creats a function SafeDiv that divides and returns the first
+-- by the second number or returns 0 if the second number is 0.
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
