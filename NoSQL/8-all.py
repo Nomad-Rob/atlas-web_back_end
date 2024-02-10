12bc8650dfd175812b55fcc3b421cf44f3cf238c
Nomad-Rob <rfarley89@gmail.com>
2024-02-10 17:44:08 -0600

    completed task 0-7 and works with mongodb. Need to fix issue with python not working in container

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Task 8. All documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
+    return mongo_collection.find()
