c3cf82dd0eef578d1d8db5f12d01b525ff60c5db
Nomad-Rob <rfarley89@gmail.com>
2024-02-20 10:08:37 -0600

    Task 0 complete. Also chmod all *.js files

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// Task 0 - Create a function named display Message that
+// prints in STDOUT the string arg
+
+function displayMessage(arg) {
+  console.log(arg);
+}
+
+module.exports = displayMessage;
