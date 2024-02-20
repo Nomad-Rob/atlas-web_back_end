477faae018921196045cd25e84bfe07745b14655
Nomad-Rob <rfarley89@gmail.com>
2024-02-20 11:05:49 -0600

    fixing linter

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 // Task 1 - Creat a program named 1-stdin.js that exes through command line
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
