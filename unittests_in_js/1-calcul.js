841e069ff792192a96ada0c0953a744ac26dc06e
Nomad-Rob <rfarley89@gmail.com>
2024-02-21 14:07:36 -0600

    Task 1 complete. Pasdsing 6 tests

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,16 @@
+// Task 1 - Upgrade the function from 0-calcul.js
+
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+}
+
+module.exports = calculateNumber;
