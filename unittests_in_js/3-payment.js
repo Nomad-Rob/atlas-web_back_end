55a97f00ef7476878add9324da2beca9da564ef8
Nomad-Rob <rfarley89@gmail.com>
2024-02-24 10:21:13 -0600

    Task 3 complete. Test pass and spies

---
+++ unittests_in_js/3-payment.js
@@ -0,0 +1,11 @@
+// Task 3 - Create function sendPaymentRequestToApi
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+  return sum;
+}
+
+module.exports = sendPaymentRequestToApi;
