b8154cfed24517131e5d9a193e8f4933a10430a9
Nomad-Rob <rfarley89@gmail.com>
2024-02-25 10:04:21 -0600

    Changed the testing for 5, needed to only use one spy to complete

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,53 +1,26 @@
 // Task 5 - Hooks
 
-const expect = require('chai').expect;
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./4-payment');
-const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should return 200 if the payment is successful', () => {
-    // Stub the calculateNumber function to always return 10
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    let spyConsole;
 
-    // Spy on console.log to verify it logs the correct message
-    const logSpy = sinon.spy(console, 'log');
+    beforeEach(() => {
+        spyConsole = sinon.spy(console, 'log');
+    });
 
-    // Execute the function under test
-    sendPaymentRequestToApi(100, 20);
+    afterEach(() => {
+        spyConsole.restore();
+    });
 
-    // Verify the stub was called with the expected arguments
-    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    // Verify that console.log was called with the expected message
-    expect(logSpy.calledWith('The total is: 10')).to.be.true;
+    it ('should log "The total is: 120" for inputs 100 and 20', () => {
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledWithExactly(spyConsole, 'The total is: 120');
+    });
 
-    // Restore the original functions to prevent side effects
-    stub.restore();
-    logSpy.restore();
-  });
-  it('should return 120 total if payment is successful', () => {
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(120);
-    const logSpy = sinon.spy(console, 'log');
-
-    sendPaymentRequestToApi(100, 20);
-
-    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(logSpy.calledWith('The total is: 120')).to.be.true;
-
-    stub.restore();
-    logSpy.restore();
-  });
-  it('should return 20 if pyament is successful', () => {
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(20);
-    const logSpy = sinon.spy(console, 'log');
-
-    sendPaymentRequestToApi(10, 10);
-
-    expect(stub.calledOnceWithExactly('SUM', 10, 10)).to.be.true;
-    expect(logSpy.calledWith('The total is: 20')).to.be.true;
-
-    stub.restore();
-    logSpy.restore();
-  });
-  
+    it ('should log "The total is: 20" for inputs 10 and 10', () => {
+        sendPaymentRequestToApi(10, 10);
+        sinon.assert.calledWithExactly(spyConsole, 'The total is: 20');
+    });
 });
